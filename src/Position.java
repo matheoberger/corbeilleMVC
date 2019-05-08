e61635a2a864f406ca9ed028f50c0f7c8f3f9d5a
Brosset-Axel <44434284+Brosset-Axel@users.noreply.github.com>
2019-05-08 16:33:20 +0200

    [0.2]Ajout classe Position et enumération Direction

---
+++ src/Position.java
@@ -0,0 +1,38 @@
+
+public class Position {
+    private double x;
+    private double y;
+    private double maxX;
+    private double maxY;
+
+    public Position(final double x, final double y, final double maxX, final double maxY) {
+        this.setX(x);
+        this.setY(y);
+        this.setMaxX(maxX);
+        this.setMaxY(maxY);
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    protected void setMaxX(final double maxX) {
+        this.maxX = maxX;
+    }
+
+    protected void setMaxY(final double maxY) {
+        this.maxY = maxY;
+    }
+
+    public void setX(final double x) {
+        this.x = x;
+    }
+
+    public void setY(final double y) {
+        this.y = y;
+    }
+}
